57baa532b194c185ba520426190892790e4b171a
fillip1984 <fillip1984@gmail.com>
2023-10-21 10:56:38 -0400

    ran npm update

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
-import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import * as route53 from "aws-cdk-lib/aws-route53";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { HostedZone } from "aws-cdk-lib/aws-route53";
 import { Tags } from "aws-cdk-lib/core";
 import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
@@ -12,35 +12,29 @@ export default {
     };
   },
   stacks(app) {
-    Tags.of(app).add("aws-hub", `${app.stage}-${app.region}`);
+    Tags.of(app).add(app.name, `${app.stage}-${app.region}`);
 
     app.stack(function Site({ stack }) {
-      const hostedZone = route53.HostedZone.fromLookup(stack, "HostedZone", {
+      const hostedZone = HostedZone.fromLookup(stack, "HostedZone", {
         domainName: "illizen.com",
       });
 
-      const certificate = new acm.DnsValidatedCertificate(
-        stack,
-        "Certificate",
-        {
-          domainName: "hub.illizen.com",
-          hostedZone,
-          region: "us-east-1",
-        }
-      );
+      const certificate = new Certificate(stack, "Certificate", {
+        domainName: `${app.name}.illizen.com`,
+      });
 
       // TODO: Should switch to storing in aws Secrets but haven't spent the time figuring out how to get it back out for primsa. See: https://docs.sst.dev/config#overview
       const DATABASE_URL = process.env.DATABASE_URL;
       if (!DATABASE_URL) {
         throw new Error(
-          "unable to find database url, it needs to be defined in a .env file or provided by your CI as DATABASE_URL...example is provided in .env.example"
+          "unable to find database url, it needs to be defined in a .env file or provided by your CI as DATABASE_URL...example is provided in .env.example",
         );
       }
 
       const site = new NextjsSite(stack, "site", {
         customDomain: {
-          domainName: "hub.illizen.com",
-          domainAlias: "www.hub.illizen.com",
+          domainName: `${app.name}.illizen.com`,
+          domainAlias: `www.${app.name}.illizen.com`,
           cdk: {
             hostedZone,
             certificate,
